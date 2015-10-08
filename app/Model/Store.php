df9734b40562cbbf805db11b495e318f9afdf7ab
Phillip Whelan <pwhelan@mixxx.org>
2015-10-08 01:22:15 -0300

    Esconder la locacion (dato muy complejo) y el login_id de listados de stores.

--- app/Model/Store.php
+++ app/Model/Store.php
@@ -4,4 +4,10 @@ namespace App\Model;
 
 class Store extends AbstractBase
 {
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = ['login_id', 'location', 'created_at', 'updated_at', 'deleted_at'];
 }
