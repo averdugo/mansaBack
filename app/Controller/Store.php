15016ecde38e06bba25ce6e3655f96b9ecfa2a3f
Phillip Whelan <pwhelan@mixxx.org>
2015-10-08 03:54:50 -0300

    Crear tiendas con locaciones (GPS).

--- app/Controller/Store.php
+++ app/Controller/Store.php
@@ -24,11 +24,29 @@ class Store implements ControllerProviderInterface
 		
 		$controller->put('/', function(Application $app, Request $req) {
 			
+			$db = $app['capsule']->connection();
+			
+			
+			if (!$req->get('lat') || !$req->get('lon'))
+			{
+				throw new \Exception("la tienda no tiene locacion");
+			}
+			
+			if (!is_numeric($req->get('lat')) || !is_numeric($req->get('lon')))
+			{
+				throw new \Exception("la locacion es invalida");
+			}
+			
+			$lat = $req->get('lat');
+			$lon = $req->get('lon');
+			
+			
 			$store = new Model\Store;
 			$store->login_id = $app['session']->get('user_id');
 			$store->address = $req->get('address');
 			$store->comuna = $req->get('comuna');
 			$store->region = $req->get('region');
+			$store->location = $db->raw("ST_GeographyFromText('SRID=4326;POINT({$lat} {$lon})')");
 			$store->save();
 			
 			return $store->toJSON();
