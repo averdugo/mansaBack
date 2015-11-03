9edfe85ddf14f382cbd8631f04bd87202e917e8d
Phillip Whelan <pwhelan@mixxx.org>
2015-11-02 23:24:20 -0300

    Automatically encode/code stores.hours column as JSON/PHP object using accesor/mutator.

--- app/Model/Store.php
+++ app/Model/Store.php
@@ -10,4 +10,14 @@ class Store extends AbstractBase
 	 * @var array
 	 */
 	protected $hidden = ['login_id', 'location', 'created_at', 'updated_at', 'deleted_at'];
+	
+	public function getHoursAttribute($value)
+	{
+		return json_decode($value);
+	}
+	
+	public function setHours($value)
+	{
+		$this->attributes['hours'] = json_encode($value);
+	}
 }
