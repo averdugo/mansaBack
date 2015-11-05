5639a0ef9210740bf2af3eb7b5d7e742da228460
Phillip Whelan <pwhelan@mixxx.org>
2015-11-04 23:17:30 -0300

    FIX: parse error (bad merge from Aldo...)

--- app/Controller/Cupon.php
+++ app/Controller/Cupon.php
@@ -146,11 +146,6 @@ class Cupon implements ControllerProviderInterface
 					);
 				});
 			}
-			if ($req->get('s'))
-			{
-				$query->where('store_id','=',$req->get('s'))
-				
-			}
 			
 			if ($req->get('s'))
 			{
