015de480fbdd2f65608f7bd80f7ac7727552a4c2
Phillip Whelan <pwhelan@mixxx.org>
2015-11-12 10:42:51 -0300

    Add email field to devices (and save it in the database).

--- app/Controller/Device.php
+++ app/Controller/Device.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use App\Model;
+
 use Rhumsaa\Uuid\Uuid;
 use Rhumsaa\Uuid\Exception\UnsatisfiedDependencyException;
 
@@ -25,18 +27,13 @@ class Device implements ControllerProviderInterface
 		
 		$controller->put('/', function(Application $app, Request $request) {
 			
-			try
-			{
-				// Generate a version 4 (random) UUID object
-				return new JsonResponse(['uuid' => uniqid()]);
-			}
-			catch (UnsatisfiedDependencyException $e)
-			{
-				// Some dependency was not met. Either the method cannot be called on a
-				// 32-bit system, or it can, but it relies on Moontoast\Math to be present.
-				throw new \Exception("server error: ".$e->getMessage());
-			}
+			$device = new Model\Device;
 			
+			$device->device_id	= uniqid();
+			$device->email		= $request->get('email');
+			$device->save();
+			
+			return new JsonResponse($device->toArray());
 		});
 		
 		return $controller;
