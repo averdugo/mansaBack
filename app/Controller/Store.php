41051709a175c5b4fb24d2074655da327f2c2d32
Phillip Whelan <pwhelan@mixxx.org>
2015-11-26 15:08:15 -0300

    FIX: NotFoundHttpException does not exist (GET /store/{$nonexistentid})..

--- app/Controller/Store.php
+++ app/Controller/Store.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 use App\Model;
 
 /******************************************************************************/
